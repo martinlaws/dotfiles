fb1681e0b73a2a3c4f875ec12ba70edf6d5f6f2d
Martin Laws <martin.laws@thinkresearch.com>
2016-03-15 14:00:15 -0400

    added nvm and rvm to zsh

---
+++ zsh/rvm.zsh
@@ -0,0 +1,3 @@
+export PATH="$PATH:/home/vagrant/.rvm/bin" # Add RVM to PATH for scripting
+
+[[ -s "/home/vagrant/.rvm/scripts/rvm" ]] && source "/home/vagrant/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
